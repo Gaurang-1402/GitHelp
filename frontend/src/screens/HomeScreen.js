ce410abf2e52702c61f715068ba8840aa9eae006
Gaurang Ruparelia <gr2159@nyu.edu>
2021-02-20 08:25:27 -0500

    Changed homescreen to show the repo projects

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import Paginate from '../components/Paginate'
-import ProductCarousel from '../components/ProductCarousel'
-import Meta from '../components/Meta'
-import { listProducts } from '../actions/productActions'
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Row, Col } from "react-bootstrap"
+import Product from "../components/Product"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import Paginate from "../components/Paginate"
+import ProductCarousel from "../components/ProductCarousel"
+import Meta from "../components/Meta"
+import { listProducts } from "../actions/productActions"
+import { Card } from "react-bootstrap"
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword
@@ -34,7 +35,7 @@ const HomeScreen = ({ match }) => {
           Go Back
         </Link>
       )}
-      <h1>Latest Products</h1>
+      <h1>Python projects</h1>
       {loading ? (
         <Loader />
       ) : error ? (
@@ -42,16 +43,85 @@ const HomeScreen = ({ match }) => {
       ) : (
         <>
           <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
+            <Col key='6030fcee26b9f63ad0b4dfa6' sm={12} md={6} lg={4} xl={3}>
+              <Card className='my-3 p-3 rounded'>
+                <Link to={`/product/6030fcee26b9f63ad0b4dfa6`}>
+                  <Card.Img src='/images/camera.jpg' variant='top' />
+                </Link>
+
+                <Card.Body>
+                  <Link to={`/product/'6030fcee26b9f63ad0b4dfa6'`}>
+                    <Card.Title as='div'>
+                      <strong>Repo name1</strong>
+                    </Card.Title>
+                  </Link>
+
+                  <Card.Text as='h3'>Some description of the repo</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+
           </Row>
           <Paginate
             pages={pages}
             page={page}
-            keyword={keyword ? keyword : ''}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
+      )}
+
+      <h1>React projects</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Row>
+            <Col key='6030fcee26b9f63ad0b4dfa6' sm={12} md={6} lg={4} xl={3}>
+              <Card className='my-3 p-3 rounded'>
+                <Link to={`6030fcee26b9f63ad0b4dfa7`}>
+                  <Card.Img src='/images/playstation.jpg' variant='top' />
+                </Link>
+
+                <Card.Body>
+                  <Link to={`/product/'6030fcee26b9f63ad0b4dfa7'`}>
+                    <Card.Title as='div'>
+                      <strong>Repo Name2</strong>
+                    </Card.Title>
+                  </Link>
+
+                  <Card.Text as='h3'>Some description of the repo</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+
+            <Col key='' sm={12} md={6} lg={4} xl={3}>
+              <Card className='my-3 p-3 rounded'>
+                <Link to={`/product/6030fcee26b9f63ad0b4dfa8`}>
+                  <Card.Img src='/images/mouse.jpg' variant='top' />
+                </Link>
+
+                <Card.Body>
+                  <Link to={`/product/'6030fcee26b9f63ad0b4dfa8'`}>
+                    <Card.Title as='div'>
+                      <strong>Repo Name3</strong>
+                    </Card.Title>
+                  </Link>
+
+                  <Card.Text as='h3'>Some description of the repository</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
           />
         </>
       )}
